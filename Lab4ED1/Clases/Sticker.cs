2a2ec5958d27b6021a7d4b0eabaedcfc6fab625b
Francisco Alonzo <franalonzo96@gmail.com>
2018-04-07 22:25:25 -0600

    Adición Diccionarios, singleton path, llaves

---
+++ Lab4ED1/Clases/Sticker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab4ED1.Clases
+{
+    public class Sticker
+    {
+        int number;
+        string NCountry;
+
+        public int Number { get => number; set => number = value; }
+        public string NCountry_ { get => NCountry; set => NCountry = value; }
+
+        public Sticker(int n, string name)
+        {
+            Number = n;
+            NCountry_ = name;
+        }
+    }
+}
